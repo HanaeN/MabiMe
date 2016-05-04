4692d495e03f8d36ef4d9572b7fad0972ab265b4
Hanae Narahashi <Hanae Narahashi>
2016-05-04 23:50:46 +0100

    Started work on PMG reading. can verify PMG type and basic structure.

--- PackReader/pmgreader.cpp
+++ PackReader/pmgreader.cpp
@@ -1,7 +1,69 @@
 #include "pmgreader.h"
 
+#include <QDebug>
+
 PMGReader::PMGReader()
 {
-
+    meshes = QList<PMG::Mesh>();
 }
 
+bool PMGReader::LoadPMG(QByteArray stream) {
+    char *data = stream.data();
+    try {
+        qDebug() << stream.length();
+        if (stream.length() > sizeof(PMG::FileHeader)) {
+            header = *(PMG::FileHeader*)&data[0];
+            uint32_t pos = sizeof(PMG::FileHeader) + 64;
+            if (strcmp(header.magic, "pmg\x00") == 0 && header.version == 258) { // check if valid
+                int meshCount = *(int*)&data[pos];
+                pos += 208 * meshCount;
+                qDebug() << meshCount << header.meshType;
+                if (header.meshType == 1) {
+                } else if (header.meshType == 5) {
+                    pos += 272 * 4;
+                } else {
+                    qDebug() << "unsupported PMG mesh type";
+                    return false;
+                }
+                qDebug() << meshCount;
+                for (int i = 0; i < meshCount; i++) {
+                    char pmMagic[4];
+                    memcpy(&pmMagic, &data[pos], 4);
+                    short pmVersion = *(short*)&data[pos + 4];
+                    pos += 6;
+                    qDebug() << pmMagic << pmVersion;
+                    if (strcmp(pmMagic, "pm!\x00") != 0) {
+                        qDebug() << "pm type not supported";
+                        return false;
+                    }
+                    if (pmVersion != 1793 && pmVersion != 2) {
+                        qDebug() << "pm version not supported:" << pmVersion;
+                        return false;
+                    }
+                    // read mesh
+                    PMG::Mesh mesh;
+                    if (pmVersion == 1793) {
+                        mesh.size = *(int*)&data[pos];
+                        pos += 4;
+                        memcpy(&mesh.boneName, &data[pos], 32);
+                        pos += 32;
+                        memcpy(&mesh.meshName, &data[pos], 128);
+                        qDebug() << mesh.boneName << mesh.meshName;
+                        // CONTINUE HERE // LOADPM17
+                    }
+                    if (pmVersion == 2) {
+
+                    }
+                    return true;
+                }
+            } else {
+                qDebug() << "unsupported PMG format";
+                return false;
+            }
+        }
+    } catch (...) {
+        qDebug() << "Failed to load PMG header";
+        return false;
+    }
+    return true;
+}
--- PackReader/pmgreader.h
+++ PackReader/pmgreader.h
@@ -1,11 +1,56 @@
 #ifndef PMGREADER_H
 #define PMGREADER_H
 
+#include <QByteArray>
+#include <QtOpenGL>
+
+namespace PMG {
+    #pragma pack(1)
+    struct FileHeader {
+        char magic[4];
+        short version;
+        int headSize;
+        char meshName[128];
+        int meshType;
+    };
+    struct Vertex {
+        float x, y, z;
+        float nx, ny, nz;
+        float r,g,b,a;
+        float u,v;
+    };
+    struct Skin {
+        int n = 0;
+        int a = 0;
+        float weight = 1;
+        int b = 1;
+    };
+    struct Mesh {
+        int size;
+        char boneName[32];
+        char meshName[128];
+        char textureName[32];
+        QMatrix4x4 minorMatrix, majorMatrix;
+        int partNo               = 0;
+        int faceVertexCount      = 0;
+        int faceCount            = 0;
+        int stripFaceVertexCount = 0;
+        int stripFaceCount       = 0;
+        int vertexCount          = 0;
+        int skinCount            = 0;
+        QList<Vertex> vertices = QList<Vertex>();
+        QList<Skin> skins = QList<Skin>();
+    };
+}
 
 class PMGReader
 {
+private:
+    PMG::FileHeader header;
+    QList<PMG::Mesh> meshes;
 public:
     PMGReader();
+    bool LoadPMG(QByteArray stream);
 };
 
 #endif // PMGREADER_H
