cb1dbf7bca6356cb4386bacef9b17eba94663886
Hanae Narahashi <Hanae Narahashi>
2016-05-07 19:40:16 +0100

    New class for reading FRM (bone) format.

---
+++ PackReader/frmreader.cpp
@@ -0,0 +1,7 @@
+#include "frmreader.h"
+
+FRMReader::FRMReader()
+{
+
+}
+
---
+++ PackReader/frmreader.h
@@ -0,0 +1,15 @@
+#ifndef FRMREADER_H
+#define FRMREADER_H
+
+
+class FRMReader
+{
+public:
+    FRMReader();
+
+signals:
+
+public slots:
+};
+
+#endif // FRMREADER_H
