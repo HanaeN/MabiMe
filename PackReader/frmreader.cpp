ae48cfa1ebd3512a999746d16155eb8598c97ef7
Hanae Narahashi <Hanae Narahashi>
2016-05-08 16:13:13 +0100

    Implemented working FRM format.

--- PackReader/frmreader.cpp
+++ PackReader/frmreader.cpp
@@ -1,10 +1,60 @@
 #include "frmreader.h"
 
+#include <QDebug>
+
 FRMReader::FRMReader()
 {
-
+    bones = QList<FRM::Bone*>();
 }
 
 bool FRMReader::LoadFRM(QByteArray stream) {
+    char *data = stream.data();
+    try {
+        qDebug() << stream.length();
+        if (stream.length() > sizeof(FRM::FileHeader)) {
+            header = *(FRM::FileHeader*)&data[0];
+            uint32_t pos = sizeof(FRM::FileHeader);
+            if (strcmp(header.magic, "pf!\x00") == 0 && header.version == 1) { // check if valid
+                qDebug() << header.bonesCount << "bones";
+                for (int n = 0; n < header.bonesCount; n++) {
+                    FRM::Bone *b = new FRM::Bone();
+                    memcpy(b, &data[pos], sizeof(FRM::Bone));
+                    qDebug() << QString::number(b->boneID) << b->name << QString::number(b->parentID);
+                    float d[16];
+                    memcpy(d, b->localToGlobal, 64);
+                    QMatrix4x4 m(d[0],  d[1],  d[2],  d[3],
+                                 d[4],  d[5],  d[6],  d[7],
+                                 d[8],  d[9],  d[10], d[11],
+                                 d[12], d[13], d[14], d[15]);
+                    memcpy(b->localToGlobal, m.data(), 64);
+                    memcpy(d, b->globalToLocal, 64);
+                    m = QMatrix4x4(d[0],  d[1],  d[2],  d[3],
+                                 d[4],  d[5],  d[6],  d[7],
+                                 d[8],  d[9],  d[10], d[11],
+                                 d[12], d[13], d[14], d[15]);
+                    memcpy(b->globalToLocal, m.data(), 64);
+                    memcpy(d, b->link, 64);
+                    m = QMatrix4x4(d[0],  d[1],  d[2],  d[3],
+                                 d[4],  d[5],  d[6],  d[7],
+                                 d[8],  d[9],  d[10], d[11],
+                                 d[12], d[13], d[14], d[15]);
+                    memcpy(b->link, m.data(), 64);
+//                    m.toTransform()
+                    pos += sizeof(FRM::Bone);
+                    bones.append(b);
+                }
+            } else {
+                qDebug() << "not a valid FRM file";
+                return false;
+            }
+        } else {
+            qDebug() << "invalid FRM size!";
+            return false;
+        }
+    } catch (...) {
+        qDebug() << "there was an error reading the FRM";
+        return false;
+    }
+
     return true;
 }
--- PackReader/frmreader.h
+++ PackReader/frmreader.h
@@ -1,13 +1,26 @@
 #ifndef FRMREADER_H
 #define FRMREADER_H
 
+#include <QByteArray>
+#include <QMatrix4x4>
+#include <QtOpenGL>
 
 namespace FRM {
-    struct Header {
+    #pragma pack(1)
+    struct FileHeader {
         char magic[4];
-        short version, bones_count;
+        short version, bonesCount;
     };
+    #pragma pack(1)
     struct Bone {
+        float globalToLocal[16], localToGlobal[16], link[16];
+        char dummy;
+        char name[31];
+        char boneID, parentID;
+        short empty;
+        QQuaternion quat1, quat2;
+    };
+    struct CleanBone {
 
     };
 }
@@ -15,8 +28,9 @@ namespace FRM {
 class FRMReader
 {
 private:
-    FRM::Header header;
+    FRM::FileHeader header;
 public:
+    QList<FRM::Bone*> bones;
     FRMReader();
     bool LoadFRM(QByteArray stream);
 };
