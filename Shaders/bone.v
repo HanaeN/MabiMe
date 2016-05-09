400e42152b4ee0194ff59b2f78539d5f82ec541c
Hanae Narahashi <Hanae Narahashi>
2016-05-09 16:22:57 +0100

    Added world space and local bone matrices.

--- Shaders/bone.v
+++ Shaders/bone.v
@@ -4,15 +4,15 @@ varying vec3 N;
 varying vec3 v;
 
 uniform mat4x4 boneMatrix;
-uniform float boneData[1000];
+uniform mat4x4 worldMatrix;
+uniform float boneWeight[1000];
 
 void main(void)
 {
   vec4 newVertex;
   v = vec3(gl_ModelViewMatrix * gl_Vertex);       
   N = normalize(gl_NormalMatrix * gl_Normal);
-  newVertex = (gl_Vertex * boneMatrix);
-//  gl_Position = gl_ModelViewProjectionMatrix * ((gl_Vertex * boneMatrix) * boneData[gl_VertexID]);
+  newVertex = (gl_Vertex * boneMatrix) * worldMatrix;
   gl_Position = gl_ModelViewProjectionMatrix * newVertex;
   gl_TexCoord[0] = gl_MultiTexCoord0;
 }       
