cd50f925dfe935bfbe3a8af5033dcf401127b88d
Hanae Narahashi <Hanae Narahashi>
2016-05-09 15:38:52 +0100

    Shader now handles outline colouring and can take in a bone matrix.

--- Shaders/bone.f
+++ Shaders/bone.f
@@ -1,18 +1,23 @@
 uniform sampler2D textureIn;
+uniform int isOutline;
 
 varying vec3 N;
 varying vec3 v;
- 
+
 void main (void)  
 {
-  vec3 L = normalize(gl_LightSource[0].position.xyz - v);   
-  vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0)  
-  vec3 R = normalize(-reflect(L,N));  
-  vec4 Iamb = gl_FrontLightProduct[0].ambient;    
-  vec4 Idiff = gl_FrontLightProduct[0].diffuse * max(dot(N,L), 0.0);
-  Idiff = clamp(Idiff, 0.0, 1.0);     
-  vec4 Ispec = gl_FrontLightProduct[0].specular * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
-  Ispec = clamp(Ispec, 0.0, 1.0); 
-  gl_FragColor = texture2D(textureIn, gl_TexCoord[0].xy).rgba *
-  (gl_FrontLightModelProduct.sceneColor + Iamb + Idiff + Ispec); 
+  if (isOutline == 1) {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  } else {
+    vec3 L = normalize(gl_LightSource[0].position.xyz - v);   
+    vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0)  
+    vec3 R = normalize(-reflect(L,N));  
+    vec4 Iamb = gl_FrontLightProduct[0].ambient;    
+    vec4 Idiff = gl_FrontLightProduct[0].diffuse * max(dot(N,L), 0.0);
+    Idiff = clamp(Idiff, 0.0, 1.0);     
+    vec4 Ispec = gl_FrontLightProduct[0].specular * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
+    Ispec = clamp(Ispec, 0.0, 1.0); 
+    gl_FragColor = texture2D(textureIn, gl_TexCoord[0].xy).rgba *
+    (gl_FrontLightModelProduct.sceneColor + Iamb + Idiff + Ispec); 
+  }
 }
--- Shaders/bone.v
+++ Shaders/bone.v
@@ -1,10 +1,18 @@
+#version 120
+
 varying vec3 N;
 varying vec3 v;
 
+uniform mat4x4 boneMatrix;
+uniform float boneData[1000];
+
 void main(void)
 {
+  vec4 newVertex;
   v = vec3(gl_ModelViewMatrix * gl_Vertex);       
   N = normalize(gl_NormalMatrix * gl_Normal);
-  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+  newVertex = (gl_Vertex * boneMatrix);
+//  gl_Position = gl_ModelViewProjectionMatrix * ((gl_Vertex * boneMatrix) * boneData[gl_VertexID]);
+  gl_Position = gl_ModelViewProjectionMatrix * newVertex;
   gl_TexCoord[0] = gl_MultiTexCoord0;
 }       
