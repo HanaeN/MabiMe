a1d90a28409ed9bd7424b30c220d2d8aadf03657
Hanae Narahashi <Hanae Narahashi>
2016-05-12 21:51:50 +0100

    Bone shader is working mostly.

--- Shaders/bone.v
+++ Shaders/bone.v
@@ -16,7 +16,7 @@ void main(void)
   vec4 newVertex;
   int bID = boneID[gl_VertexID];
   newVertex =  ((boneMatrix[0] * boneWeight[gl_VertexID]) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
-  newVertex += ((boneMatrix[1] * (1.0 - boneWeight[gl_VertexID])) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
+  newVertex += ((boneMatrix[bID] * (1.0 - boneWeight[gl_VertexID])) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
   v = vec3(gl_ModelViewMatrix * newVertex);
   gl_Position = (gl_ModelViewProjectionMatrix * vec4(newVertex.xyz, 1.0));
   gl_TexCoord[0] = gl_MultiTexCoord0;
