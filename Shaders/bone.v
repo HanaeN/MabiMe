764ef26b3c1b0b9ad71dd4c03a1a348a64521120
Hanae Narahashi <Hanae Narahashi>
2016-05-16 17:53:31 +0100

    Temporary disabling of bone calcs until new code is done

--- Shaders/bone.v
+++ Shaders/bone.v
@@ -13,10 +13,13 @@ uniform int boneID[430];
 void main(void)
 {
   N = normalize(gl_NormalMatrix * gl_Normal);
-  vec4 newVertex;
+  vec4 newVertex = gl_Vertex;
   int bID = boneID[gl_VertexID];
-  newVertex =  ((boneMatrix[0] * boneWeight[gl_VertexID]) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
-  newVertex += ((boneMatrix[bID] * (1.0 - boneWeight[gl_VertexID])) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
+
+  newVertex =  (boneMatrix[0] * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
+
+//  newVertex =  ((boneMatrix[0] * boneWeight[gl_VertexID]) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
+//  newVertex += ((boneMatrix[bID] * (1.0 - boneWeight[gl_VertexID])) * worldMatrix * vec4(gl_Vertex.xyz, 1.0));
   v = vec3(gl_ModelViewMatrix * newVertex);
   gl_Position = (gl_ModelViewProjectionMatrix * vec4(newVertex.xyz, 1.0));
   gl_TexCoord[0] = gl_MultiTexCoord0;
